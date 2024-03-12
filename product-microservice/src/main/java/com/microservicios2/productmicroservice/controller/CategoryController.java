59d40679979e77302b9860e0d02b65f03ced3f11
gabriel <gabrielferv95@gmail.com>
2024-03-12 10:25:50 -0500

    second change

--- product-microservice/src/main/java/com/microservicios2/productmicroservice/controller/CategoryController.java
+++ product-microservice/src/main/java/com/microservicios2/productmicroservice/controller/CategoryController.java
@@ -2,10 +2,7 @@ package com.microservicios2.productmicroservice.controller;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,18 +14,8 @@ public class CategoryController {
 
     @Value("${testProp}")
     private String testProp;
-    private static final Logger logger = LoggerFactory.getLogger(CategoryController.class);
 
-    @GetMapping("test-prop")
-    public String getTestProp() {
-        try {
-            // Intenta hacer algo con la propiedad
-            System.out.println("Value of testProp: " + testProp);
-            return this.testProp;
-        } catch (Exception e) {
-            // Captura la excepción y muestra el mensaje de error
-            System.err.println("Error: " + e.getMessage());
-            throw e;
-        }
+    public String getTestProp(){
+        return this.testProp;
     }
 }
