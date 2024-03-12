7cde2e7b8934b464c9c9fd85ffa19bf512d97c9e
gabriel <gabrielferv95@gmail.com>
2024-03-12 07:44:22 -0500

    configuration resfresScope2

--- product-microservice/src/main/java/com/microservicios2/productmicroservice/controller/CategoryController.java
+++ product-microservice/src/main/java/com/microservicios2/productmicroservice/controller/CategoryController.java
@@ -2,7 +2,10 @@ package com.microservicios2.productmicroservice.controller;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,10 +15,20 @@ import org.springframework.web.bind.annotation.RestController;
 @Setter
 public class CategoryController {
 
-    @Value("${app.testProp}")
+    @Value("${testProp}")
     private String testProp;
+    private static final Logger logger = LoggerFactory.getLogger(CategoryController.class);
 
-    public String getTestProp(){
-        return this.testProp;
+    @GetMapping("test-prop")
+    public String getTestProp() {
+        try {
+            // Intenta hacer algo con la propiedad
+            System.out.println("Value of testProp: " + testProp);
+            return this.testProp;
+        } catch (Exception e) {
+            // Captura la excepción y muestra el mensaje de error
+            System.err.println("Error: " + e.getMessage());
+            throw e;
+        }
     }
 }
